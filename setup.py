e39e4943a93028f31f708f6a25cbfd61a52f2878
zhaoliming <zhaoliming@daixiaomi.com>
2018-11-13 09:53:10 +0800

    Update project name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = path.abspath(path.dirname(__file__))
 #     long_description = f.read()
 
 setup(
-    name='tsunami',
+    name='tsunamiweb',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
